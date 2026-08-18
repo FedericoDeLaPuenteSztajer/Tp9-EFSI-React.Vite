604bcda4d665fe13ed2392b867c32a105b0b99c3
Federico de la Puente Sztajer <49319314@est.ort.edu.ar>
2026-08-18 15:14:23 -0300

    mayor general update. Can now search for pokemon data

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,48 @@ import pokeAPI from './modules/PokeAPI.js';
 
 function App() {
 
-  const async SearchSkorupi = () => {
-    const response = await pokeAPI.get("");
+  const [pokemon, setPokemon] = useState({});
 
-    return response.name
+  const SearchPokemon = async () => {
+    const response = await pokeAPI.get("https://pokeapi.co/api/v2/pokemon/ditto");
+    const pokeData = response.data;
+    const statsList = []
+
+    pokeData.stats.forEach(pokeStat => {
+      statsList.push({
+        name: pokeStat.stat.name,
+        basePoints: pokeStat.base_stat
+      })
+    });
+
+    setPokemon({
+      name: pokeData.name[0].toUpperCase() + pokeData.name.slice(1),
+      sprite: (pokeData.sprites.other["official-artwork"])["front_shiny"],
+      types: pokeData.types[0].type.name + (pokeData.types[1] ? "/" + pokeData.types[1].type.name : ""),
+      statsList: statsList
+    });
   }
-
 
   return (
     <>
-
+      {pokemon ?
+        (<div>
+          <h1>{pokemon.name}</h1>
+          <img src={pokemon.sprite}></img>
+          <h2>{pokemon.types}</h2>
+          <ul>
+            {pokemon.statsList?.map(stat => (
+              <li key={stat.name}>
+                {stat.name}: {stat.basePoints}
+              </li>
+            ))}
+          </ul>
+          <button onClick={() => SearchPokemon()}></button>
+        </div>
+        ) : (
+          <div>
+            <h1>Search a Pokemon</h1>
+          </div>)}
     </>
   )
 }
